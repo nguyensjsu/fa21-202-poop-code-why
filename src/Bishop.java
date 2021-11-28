8deeb318759bca6d236f7fa411ace38e9f1cbfe4
Chinmay Shukla <89881906+Chinmayshukla221@users.noreply.github.com>
2021-11-28 00:24:53 -0800

    Bishop's modified code
    Bishop's changed code according to the strategy pattern

--- src/Bishop.java
+++ src/Bishop.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 
 /**
  * Bishop.
- * @author Kevin Wehde
+ * @author Chinmay,Parmeet, Hespeak, Pratik
  * @version25 19.11.2020
  */
 public class Bishop extends Piece {
     
+    IMoveStrategy currStrategy;
     Bishop(int cd) {
         super(cd);  
         if (this.cd == 1) setImage("bishop-black-50.png");
         else setImage("bishop-white-50.png");
+        currStrategy = new BishopStrategy(this);
     }
  
     public List<Position> getLegalPositions(){
-        List<Position> list = new ArrayList<Position>();
-        int d = 1;      
+        List<Position> list = currStrategy.getLegalPositions();
+        
+        /*int d = 1;      
         while (getX() + d < 8 && getY() + d < 8 && !isOwnPieceAtOffset(d, d)) {
             list.add(new Position(getX()+d, getY()+d));
             if (isPieceAtOffset(d, d)) break;
@@ -40,7 +43,7 @@ public class Bishop extends Piece {
             list.add(new Position(getX()-d, getY()-d));
             if (isPieceAtOffset(-d,-d)) break;
             d++;
-        }
+        }*/
         return list;
     } 
     
