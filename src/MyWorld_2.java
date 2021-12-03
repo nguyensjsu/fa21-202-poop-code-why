fa3801b697764559d11d326b4b755a21f85028a6
Parmeet Singh <sainijoy.parmeet54@gmail.com>
2021-12-02 22:25:55 -0800

    Added black, white checkmate states, and end game worlds

--- src/MyWorld_2.java
+++ src/MyWorld_2.java
@@ -27,6 +27,8 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
     private CheckState checkState;
     private CheckmateState checkmateState;
     private IFactory ChessFactory;
+    private WhiteCheckmateState whiteCMState;
+    private BlackCheckmateState blackCMState;
 
     public MyWorld_2() {    
         super(8, 8, 50);
@@ -36,8 +38,8 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
         ChessFactory.buildBoard(this);
         
         normalState = new NormalState(this);
-        checkState = new CheckState(this);
-        checkmateState = new CheckmateState(this);
+        blackCMState = new BlackCheckmateState(this);
+        whiteCMState = new WhiteCheckmateState(this);
         
         state = normalState; // Starts with Normal State
 
@@ -48,10 +50,34 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
 
     public void act() {
         //movePiece();
-        
-        move(); // Using this instead to initiate the current state's appropriate method
+        stateMethod();// Using this instead to initiate the current state's appropriate method
+    }
+    
+    /**
+     * Uses the state's move method
+     */
+    public void stateMethod(){
+        if(state == normalState)
+            state.move();
+        else if(state == whiteCMState || state == blackCMState)
+            state.endGame();
+    }
+    
+    /**
+     * Change state of the board
+     * 
+     * @param state, change to this state
+     */
+    public void changeState(IBoardState state){
+        this.state= state;
     }
 
+    /**
+     * Selects the piece that cursor selects
+     * 
+     * @param p , piece to be moved
+     * @param cd
+     */
     public boolean select(Piece p, int cd) {
         if (cd == turn) {
             if (isPieceSelected) {
@@ -66,32 +92,19 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
         }
     }
 
+    /**
+     * Shows the legal moves for the selected piece
+     */
     private void showLegalMoves() {
         List<Position> legalPositions = selectedPiece.getLegalPositions();
         for (Position p: legalPositions) {
             addObject(new HighlightPosition(), p.getX(), p.getY());
         }
     }
-    
-    /**
-     * Uses the state's move method
-     * If normal state, uses MovePiece()
-     * If check state, uses.....
-     * If checkmate state, uses ..... 
-     */
-    public void move(){
-        state.move();
-    }
-    
-    /**
-     * Change state
-     * 
-     * @param state, change to this state
-     */
-    public void changeState(IBoardState state){
-        this.state= state;
-    }
 
+    /**
+     * Moves the pieces in the world
+     */
     public void movePiece() {
         for (HighlightPosition p: getObjects(HighlightPosition.class)) {
             if (Greenfoot.mouseClicked(p)) {
@@ -99,7 +112,9 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
                 List<Piece> l = getObjectsAt(targetPosition.getX(), targetPosition.getY(), Piece.class);
                 selectedPiece.move(targetPosition);
                 if (l.size() > 0) capture(l.get(0));
+
                 unselectPiece(selectedPiece);
+                
                 changeTurn();
             }
         }
@@ -146,11 +161,18 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
         ElPassantPawns.clear();
     }
     
-    /**
-     * Ends the game when checkmate
-     * Will implement display on screen later
-     */
-    public void end(){
-        //System.out.println("Game Finished");
+    /*
+    public void end()
+    {
+        //String whoWon; 
+        
+        if(state== whiteCMState)
+            Greenfoot.setWorld(new WhiteWonWorld());
+        else if(state == blackCMState)
+            Greenfoot.setWorld(new BlackWonWorld());
+        //showText("Black Won!",4,3);
+        
+        //Greenfoot.stop();
     }
+    */
 }
