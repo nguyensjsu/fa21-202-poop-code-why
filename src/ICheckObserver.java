57bde37f75cbc11ad54920c5bf236029fe86e684
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-12-04 05:04:27 -0800

    check observer

---
+++ src/ICheckObserver.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class ICheckObserver here.
+ * 
+ * @author (Pratik,Chinmay,Parmeet, Muhammmed Mahmood) 
+ * @version (a version number or a date)
+ */
+public interface ICheckObserver  
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * 
+     * 
+     * Observer to observer checkmateevent
+     */
+    public void checkEvent();
+    
+
+}
---
+++ src/checkObserver.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  
+/**
+ * Write a description of class checkObserver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class checkObserver implements ICheckObserver
+{
+    
+    MyWorld_2 myWorld ;
+    /**
+     * Constructor for objects of class checkObserver
+     */
+    public checkObserver(MyWorld_2 mw)
+    {
+        this.myWorld = mw;
+    }
+
+     public void checkEvent(){
+                            
+                     
+    };
+}
