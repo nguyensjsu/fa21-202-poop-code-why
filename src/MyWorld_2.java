6b59a8a8d464b94ce9f0942a7062bfed8d598c4d
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-12-04 05:06:06 -0800

    feat:check

--- src/MyWorld_2.java
+++ src/MyWorld_2.java
@@ -12,14 +12,19 @@ import java.util.ArrayList;
  * @author Kevin Wehde 
  * @version25 19.11.2020
  */
-public class MyWorld_2 extends World implements IElPassantObserver,IElPassantClearSubject,IStateSubject{  // PromoteObserver, subject for elpassant
+public class MyWorld_2 extends World implements IElPassantObserver,IElPassantClearSubject,IStateSubject,ICheckSubject{  // PromoteObserver, subject for elpassant
 
     boolean isPieceSelected;
     Piece selectedPiece = new DummyPiece();
     ArrayList<IMoveStrategy> ElPassantPawns;
     int turn; //1 is Black, -1 is White
+    ArrayList<Piece> BlackPieces;
+    ArrayList<Piece> WhitePieces;
+    King BlackKing = new King(1);
+    King WhiteKing =  new King(-1);
     
     private IStateObserver checkMateObserver;
+    private ICheckObserver checkObserver;
 
     //current state
     private IBoardState state;
@@ -34,14 +39,17 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
         super(8, 8, 50);
         ElPassantPawns = new ArrayList<IMoveStrategy>();
 
-        ChessFactory = new RandomFactory();
+        ChessFactory = new RegularFactory();
         ChessFactory.buildBoard(this);
         
         normalState = new NormalState(this);
         blackCMState = new BlackCheckmateState(this);
         whiteCMState = new WhiteCheckmateState(this);
 
+        //register checkMateObserver
         this.registerCheckMateObserver(new checkMateObserver(this));
+        this.registerCheckObserver(new checkObserver(this));
+ 
         
         state = normalState; // Starts with Normal State
 
@@ -66,7 +74,6 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
         if(state == normalState)
         {
             state.move();
-            state.endGame();
         }
         else if(state == whiteCMState || state == blackCMState)
             state.endGame();
@@ -116,6 +123,8 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
      * If king is captured, the game ends
      */
     public void movePiece() {
+        //WhiteKing.setImage("king-white-50.png");
+        //BlackKing.setImage("king-black-50.png");
         for (HighlightPosition p: getObjects(HighlightPosition.class)) {
             if (Greenfoot.mouseClicked(p)) {
                 Position targetPosition = new Position(p);
@@ -124,14 +133,17 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
                 
                 if (l.size() > 0) {
                     
-                    capture(l.get(0));
+                 capture(l.get(0));
                    
-
                 }
-
+                
                 unselectPiece(selectedPiece);
                 
                 changeTurn();
+                
+                // notify checkobserver of moving pieces
+                this.notifyCheckObserver();
+                
             }
         }
     }
@@ -228,8 +240,81 @@ public class MyWorld_2 extends World implements IElPassantObserver,IElPassantCle
             this.checkMateObserver.checkmateEvent();
     };
     
+     
+    public void registerCheckObserver( ICheckObserver obj ){
+        this.checkObserver = obj;
+    } ;
+    
+    public void removeCheckObserver( ICheckObserver obj ){
+        this.checkObserver = null;
+    } ;
+    
+     public void notifyCheckObserver( ){
+         if ( this.checkObserver != null )
+         {
+            Boolean flag = verifyCheck();
+            //System.out.print(flag);
+            if (flag ){
+                this.checkObserver.checkEvent();
+            }
+            
+        }
+    } ;
     
     
+    public Boolean verifyCheck(){
+        getAllActor()  ; 
+        
+        for( King k :  getObjects(King.class)){
+            if (k.cd == 1)
+            BlackKing = k;
+            else if  (k.cd == -1)
+            WhiteKing = k;
+        }
+        List<Position> list = new ArrayList<Position>();
+        if (turn == -1 )
+        {
+            //its white's turn check all the black pieces
+            for (Piece p : BlackPieces){
+                //get all the legal positions of black pieces
+                list.addAll(p.getLegalPositions());
+                
+            }
+            for (Position p : list){
+                if (p.getX()==WhiteKing.getX() && p.getY()==WhiteKing.getY()){
+                    WhiteKing.setImage("WhiteCheck.jpg");
+                return true;
+            }
+            }
+        }
+        else if (turn == 1 )
+        {
+             //its black's turn check all the white pieces
+             for (Piece p : WhitePieces){
+                 //get all the legal positions of  white pieces
+                 list.addAll(p.getLegalPositions());
+            }
+            for (Position p : list){
+                if (p.getX()==BlackKing.getX() && p.getY()==BlackKing.getY()){
+                BlackKing.setImage("checkBlack.jpeg");
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+    
+    public void getAllActor(){
+        WhitePieces = new ArrayList<Piece>();
+        BlackPieces = new ArrayList<Piece>();
+        for ( Piece p :  getObjects(Piece.class)){
+         if (p.cd == 1)
+            BlackPieces.add(p);
+         else
+             WhitePieces.add(p);
+    }
+}
     /*
     public void end()
     {
